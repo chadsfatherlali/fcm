c8c0d41d0c5f7292f94fd4a4daa1bca0eae729c3
Santiago Sánchez García <santiago.sanchez@webfg.com>
2018-03-19 13:25:59 +0100

    En la rama master Cambios a ser confirmados: 	modificado:     config.xml 	modificado:     package-lock.json 	modificado:     package.json 	modificado:     src/app/app.module.ts 	modificado:     src/pages/home/home.html 	modificado:     src/pages/home/home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public text:String = '';
+  public engine:any = null;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private storage: Storage
+  ) {
+    let _self = this
+  
+    _self.storage.ready().then(driver => {
+      _self.engine = JSON.stringify(driver)
+    });
+    _self.storage.set('fcmDB', 'Santiago');
+    
+    setTimeout(() => {
+      _self.storage.get('fcmDB').then(value => {
+        _self.text = value
+      })
+    }, 3000) 
   }
-
 }
